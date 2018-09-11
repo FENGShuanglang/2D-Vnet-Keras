e1d351752fa53552af6e7eb7a9be9171ff8c0198
Fengshuanglang <38462831+FENGShuanglang@users.noreply.github.com>
2018-09-11 10:37:42 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,13 +5,14 @@ import skimage.io as io
 import skimage.transform as trans
 import numpy as np
 import keras.backend as K
+from keras.utils import multi_gpu_model
 #from keras import optimizers
 #from keras.utils import plot_model#使用plot_mode时打开
 from keras.models import Model
-from keras.layers import Conv2D,PReLU,Conv2DTranspose,add,concatenate,Input,Dropout
+from keras.layers import Conv2D,PReLU,Conv2DTranspose,add,concatenate,Input,Dropout,BatchNormalization,Activation
 from keras.optimizers import Nadam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras import backend as keras
+#from keras import backend as keras
 
 def dice_coef(y_true, y_pred, smooth, thresh):
     #y_pred =K.cast((K.greater(y_pred,thresh)), dtype='float32')#转换为float型
@@ -19,7 +20,7 @@ def dice_coef(y_true, y_pred, smooth, thresh):
     y_true_f =y_true# K.flatten(y_true)
     y_pred_f =y_pred# K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f,axis=(0,1,2))
-    denom =K.sum(K.square(y_true_f),axis=(0,1,2)) + K.sum(K.square(y_pred_f),axis=(0,1,2))
+    denom =K.sum(y_true_f,axis=(0,1,2)) + K.sum(y_pred_f,axis=(0,1,2))
     return K.mean((2. * intersection + smooth) /(denom + smooth))
 
 def dice_loss(smooth, thresh):
@@ -36,14 +37,14 @@ def resBlock(conv,stage,keep_prob,stage_num=5):#收缩路径
     inputs=conv
     
     for _ in range(3 if stage>3 else stage):
-        conv=PReLU()(Conv2D(16*(2**(stage-1)), 5, activation = None, padding = 'same', kernel_initializer = 'he_normal')(conv))
+        conv=PReLU()(BatchNormalization()(Conv2D(16*(2**(stage-1)), 5, activation = None, padding = 'same', kernel_initializer = 'he_normal')(conv)))
         #print('conv_down_stage_%d:' %stage,conv.get_shape().as_list())#输出收缩路径中每个stage内的卷积
     conv_add=PReLU()(add([inputs,conv]))
     #print('conv_add:',conv_add.get_shape().as_list())
     conv_drop=Dropout(keep_prob)(conv_add)
     
     if stage<stage_num:
-        conv_downsample=PReLU()(Conv2D(16*(2**stage), 2, strides=(2, 2),activation = None, padding = 'same', kernel_initializer = 'he_normal')(conv_drop))
+        conv_downsample=PReLU()(BatchNormalization()(Conv2D(16*(2**stage), 2, strides=(2, 2),activation = None, padding = 'same', kernel_initializer = 'he_normal')(conv_drop)))
         return conv_downsample,conv_add#返回每个stage下采样后的结果,以及在相加之前的结果
     else:
         return conv_add,conv_add#返回相加之后的结果，为了和上面输出保持一致，所以重复输出
@@ -53,11 +54,11 @@ def up_resBlock(forward_conv,input_conv,stage):#扩展路径
     conv=concatenate([forward_conv,input_conv],axis = -1)
     print('conv_concatenate:',conv.get_shape().as_list())
     for _ in range(3 if stage>3 else stage):
-        conv=PReLU()(Conv2D(16*(2**(stage-1)), 5, activation = None, padding = 'same', kernel_initializer = 'he_normal')(conv))
+        conv=PReLU()(BatchNormalization()(Conv2D(16*(2**(stage-1)), 5, activation = None, padding = 'same', kernel_initializer = 'he_normal')(conv)))
         print('conv_up_stage_%d:' %stage,conv.get_shape().as_list())#输出扩展路径中每个stage内的卷积
     conv_add=PReLU()(add([input_conv,conv]))
     if stage>1:
-        conv_upsample=PReLU()(Conv2DTranspose(16*(2**(stage-2)),2,strides=(2, 2),padding='valid',activation = None,kernel_initializer = 'he_normal')(conv_add))
+        conv_upsample=PReLU()(BatchNormalization()(Conv2DTranspose(16*(2**(stage-2)),2,strides=(2, 2),padding='valid',activation = None,kernel_initializer = 'he_normal')(conv_add)))
         return conv_upsample
     else:
         return conv_add
@@ -66,13 +67,13 @@ def vnet(pretrained_weights = None,input_size = (256,256,1),num_class=1,is_train
     keep_prob = 1.0 if is_training else 1.0#不使用dropout
     features=[]
     input_model = Input(input_size)
-    x=PReLU()(Conv2D(16, 5, activation = None, padding = 'same', kernel_initializer = 'he_normal')(input_model))
+    x=PReLU()(BatchNormalization()(Conv2D(16, 5, activation = None, padding = 'same', kernel_initializer = 'he_normal')(input_model)))
     
     for s in range(1,stage_num+1):
         x,feature=resBlock(x,s,keep_prob,stage_num)#调用收缩路径
         features.append(feature)
         
-    conv_up=PReLU()(Conv2DTranspose(16*(2**(s-2)),2,strides=(2, 2),padding='valid',activation = None,kernel_initializer = 'he_normal')(x)) 
+    conv_up=PReLU()(BatchNormalization()(Conv2DTranspose(16*(2**(s-2)),2,strides=(2, 2),padding='valid',activation = None,kernel_initializer = 'he_normal')(x)))
     
     for d in range(stage_num-1,0,-1):
         conv_up=up_resBlock(features[d-1],conv_up,d)#调用扩展路径
@@ -87,8 +88,9 @@ def vnet(pretrained_weights = None,input_size = (256,256,1),num_class=1,is_train
     model=Model(inputs=input_model,outputs=conv_out)
     print(model.output_shape)
     
+
     model_dice=dice_loss(smooth=1e-5,thresh=0.5)
-    model.compile(optimizer = Nadam(lr = 2e-3), loss = model_dice, metrics = ['accuracy'])
+    model.compile(optimizer = Nadam(lr = 2e-4), loss = model_dice, metrics = ['accuracy'])
     
     #plot_model(model, to_file='model.png')
     if(pretrained_weights):
